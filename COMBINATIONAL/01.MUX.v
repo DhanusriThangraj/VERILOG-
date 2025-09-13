474ced9364265fb273b13898131f70aab0d516ca
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-09-13 13:08:38 +0530

    Update 01.MUX.v

--- COMBINATIONAL/01.MUX.v
+++ COMBINATIONAL/01.MUX.v
@@ -35,3 +35,20 @@ module mux_tb;
     #5;$finish;
   end
 endmodule
+
+
+//output
+Compiler version U-2023.03-SP2_Full64; Runtime version U-2023.03-SP2_Full64;  Sep 13 03:37 2025
+Time	          SEl	A	B	C	D	Y	
+0		xx	x	x	x	x	x	
+10		11	1	0	1	0	0	
+15		10	1	0	1	0	1	
+20		00	1	0	1	0	1	
+25		01	1	0	1	0	0	
+$finish called from file "testbench.sv", line 20.
+$finish at simulation time                   30
+           V C S   S i m u l a t i o n   R e p o r t 
+Time: 30 ns
+CPU Time:      0.430 seconds;       Data structure size:   0.0Mb
+Sat Sep 13 03:37:57 2025
+Done
