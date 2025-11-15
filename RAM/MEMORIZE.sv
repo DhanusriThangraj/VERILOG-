3e596a68372463e8b9ce4ed338ea3807dd0fb555
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-11-16 01:19:23 +0530

    Create MEMORIZE.sv

---
+++ RAM/MEMORIZE.sv
@@ -0,0 +1,23 @@
+SRAM (single-port)
+
+ONE array
+
+ONE port → read OR write
+-----------------------------------------------------------
+-----------------------------------------------------------
+Simple DPRAM
+
+ONE array
+
+Port A = write
+
+Port B = read
+-----------------------------------------------------------
+-----------------------------------------------------------
+True DPRAM
+
+ONE array
+
+Port A = read/write
+
+Port B = read/write
