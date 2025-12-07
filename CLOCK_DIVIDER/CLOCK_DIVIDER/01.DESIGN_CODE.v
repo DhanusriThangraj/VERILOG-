7bc3ac1523b70bde9804b789dfa65496831c340b
DHANUSRI THANGARAJ <146316804+DhanusriThangraj@users.noreply.github.com>
2025-12-07 22:27:46 +0530

    Create 01.DESIGN_CODE.v

---
+++ CLOCK_DIVIDER/CLOCK_DIVIDER/01.DESIGN_CODE.v
@@ -0,0 +1,26 @@
+// Code your design here
+
+module clock_divider #(parameter system_freq=100000000,required_freq=50_000)//50kilohz
+(input clk,reset,
+ output reg out);
+  
+  localparam divider=(system_freq/(required_freq*2));
+  reg [$clog2(divider)-1:0]count;
+
+  always @(posedge clk)begin
+    if(reset)begin
+      out<=0;
+      count<=0;
+    end
+   else begin
+     if(count==divider-1)begin
+       count<=0;
+       out<=~out;
+     end
+else begin
+   count<=count+1;
+
+end
+   end
+  end
+endmodule
